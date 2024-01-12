84610859cd059d00d14caed5a764ff80ff92ffc0
Dawid <ferero546@gmail.com>
2024-01-12 13:25:25 +0100

    Create Produkt.java

---
+++ Zad1/Produkt.java
@@ -0,0 +1,14 @@
+public class Produkt {
+    private String producent;
+    private String nazwa;
+    private double cena;
+    void setProducent(String producent){
+        this.producent = producent;
+    }
+    void setNazwa(String nazwa){
+        this.nazwa = nazwa;
+    }
+    void setCena(double cena){
+        this.cena = cena;
+    }
+}
